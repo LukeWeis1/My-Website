f8492cc2446acff915b552541bf41905f57f2223
LukeWeis1 <lukeweis@outlook.com>
2025-07-18 23:18:25 -0400

    Reworked Experience Flip Card Sizing Logic as part of ipad fixes

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,5 +1,5 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { AfterViewInit, ChangeDetectorRef, Component, ElementRef, HostListener, QueryList, ViewChildren } from '@angular/core';
 
 @Component({
   selector: 'app-experience',
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   templateUrl: './experience.component.html',
   styleUrl: './experience.component.scss'
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements AfterViewInit {
   experienceCards = [
     {
       title: 'Software Developer Co-op',
@@ -21,7 +21,8 @@ export class ExperienceComponent {
         'Collaborated closely with cross-functional teams in an Agile environment, participating in sprint planning, daily stand-ups, and code reviews.'
 
       ],
-      flipped: false
+      flipped: false,
+      heights: { frontHeight: 0, backHeight: 0, maxHeight: 0 }
     },
     {
       title: 'Software QA Co-op',
@@ -36,7 +37,8 @@ export class ExperienceComponent {
         'Conducted bi-weekly website testing to ensure functionality and performance.',
         'Collaborated with a team of 3 QAs and 10+ developers to efficiently find and resolve defects.'
       ],
-      flipped: false
+      flipped: false,
+      heights: { frontHeight: 0, backHeight: 0, maxHeight: 0 }
     },
     {
       title: 'Games Supervisor',
@@ -50,11 +52,101 @@ export class ExperienceComponent {
         'Cooperated with a team of 4 to plan and run 5 associate events in the 2023 season',
         'Conducted interviews and processed 25+ newly hired associates, taught training class to 50+ new associates in 2023'
       ],
-      flipped: false
+      flipped: false,
+      heights: { frontHeight: 0, backHeight: 0, maxHeight: 0 }
     }
   ];
 
+  @ViewChildren('frontContent') frontContents!: QueryList<ElementRef>;
+  @ViewChildren('backContent') backContents!: QueryList<ElementRef>;
+
+  private observers: MutationObserver[] = [];
+
+  private debouncedCalculate = this.debounce(() => this.calculateHeights(), 50);
+
+  constructor(private cdr: ChangeDetectorRef) { }
+
+  ngAfterViewInit() {
+    const elementsToObserve = [
+      ...this.frontContents.toArray(),
+      ...this.backContents.toArray()
+    ];
+
+    elementsToObserve.forEach(elementRef => {
+      const observer = new MutationObserver(() => {
+
+        this.debouncedCalculate();
+      });
+
+      observer.observe(elementRef.nativeElement, {
+        childList: true,
+        subtree: true,
+        characterData: true
+      });
+
+      this.observers.push(observer);
+    });
+    setTimeout(() => this.debouncedCalculate(), 100);
+  }
+
+  ngOnDestroy() {
+    this.observers.forEach(observer => observer.disconnect());
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this.debouncedCalculate();
+  }
+
+  calculateHeights() {
+
+    this.experienceCards.forEach(card => card.heights.maxHeight = 0);
+
+    this.cdr.detectChanges();
+
+    requestAnimationFrame(() => {
+      let overallMaxHeight = 0;
+
+      this.backContents.forEach((backEl, index) => {
+        const frontEl = this.frontContents.get(index);
+        if (frontEl) {
+          const frontHeight = frontEl.nativeElement.scrollHeight;
+          const backHeight = backEl.nativeElement.scrollHeight;
+          const cardMaxHeight = Math.max(frontHeight, backHeight);
+
+          if (cardMaxHeight > overallMaxHeight) {
+            overallMaxHeight = cardMaxHeight;
+          }
+        }
+      });
+
+      if (overallMaxHeight > 0) {
+
+        const finalHeight = overallMaxHeight + 60;
+        this.experienceCards.forEach(card => {
+          card.heights.maxHeight = finalHeight;
+        });
+        this.cdr.detectChanges();
+      }
+
+      if (this.observers.length > 0) {
+        this.observers.forEach(observer => observer.disconnect());
+        this.observers = [];
+      }
+    });
+  }
+
   flipCard(card: any) {
     card.flipped = !card.flipped;
+  }
+
+  private debounce(func: Function, delay: number) {
+    let timeoutId: any;
+    return (...args: any[]) => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func.apply(this, args);
+      }, delay);
+    };
   }
 }
